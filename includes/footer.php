8a5a42e805e33ebf04c3990e8df0e1f52797efb1
Shreyash Jangam <69251229+sjhack67@users.noreply.github.com>
2022-05-19 22:39:28 +0530

    uploaded required files

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p><strong>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By Shreyash Jangam</strong></p>
+        </div>
+</footer>
